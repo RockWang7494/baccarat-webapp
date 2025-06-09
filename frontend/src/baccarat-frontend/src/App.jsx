234403ad25e3a53a0b48c81e5e0db744b66ff201
RockWang7494 <kane7494@gmail.com>
2025-06-09 23:56:29 +0800

    Add files via upload

---
+++ frontend/src/baccarat-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BaccaratPredictor from "./BaccaratPredictor";
+
+function App() {
+  return (
+    <div className="p-4">
+      <BaccaratPredictor />
+    </div>
+  );
+}
+
+export default App;
