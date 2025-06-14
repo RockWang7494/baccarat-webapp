57416553f4d9a29078986b6f20170b0fc0f5500c
RockWang7494 <kane7494@gmail.com>
2025-06-14 18:19:27 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,1 @@
-import React from "react";
-import BaccaratPredictor from "./BaccaratPredictor";
-
-function App() {
-  return (
-    <div className="p-4">
-      <BaccaratPredictor />
-    </div>
-  );
-}
-
-export default App;
+import BaccaratPredictor from './BaccaratPredictor';
