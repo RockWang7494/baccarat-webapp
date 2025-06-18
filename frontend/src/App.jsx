972c0e5f1ea75dd28acce6ef200c6178b8e51e8b
RockWang <kane7494@gmail.com>
2025-06-18 23:31:43 +0800

    新增 src 資料夾與基本 React 檔案

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontSize: "1.5rem", fontWeight: "bold" }}>
+      Hello Render!
+    </div>
+  );
+}
