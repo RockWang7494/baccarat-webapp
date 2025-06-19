719014ef54ee304b9b8cf01ac97e8e4385709b07
RockWang7494 <kane7494@gmail.com>
2025-06-19 12:58:56 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './index.css'; // <-- 加這行
+import './index.css'; // ← Tailwind CSS 樣式匯入
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
