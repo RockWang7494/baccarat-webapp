13e320fe5392d05180d035138b7f9f0f479cd900
RockWang7494 <kane7494@gmail.com>
2025-06-19 12:55:57 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 export default function App() {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-3xl font-bold text-green-500">
-        Tailwind ✔️ 成功啟動！
-      </h1>
-      <p className="mt-2 text-gray-500">這是測試文字</p>
+    <div className="text-3xl font-bold text-blue-500 p-4">
+      Tailwind 整合成功！
     </div>
   );
 }
