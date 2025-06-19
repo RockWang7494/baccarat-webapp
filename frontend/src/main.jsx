8a3d1ac1a0904d3bfc9863d57855091a1ed6f88b
RockWang7494 <kane7494@gmail.com>
2025-06-19 12:42:59 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css"; // 👈 加這行才能讓 Tailwind 生效
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
