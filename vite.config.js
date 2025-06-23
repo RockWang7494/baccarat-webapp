11957df2ed83b045777fec9eedb68b47a42b01d7
RockWang <kane7494@gmail.com>
2025-06-23 20:51:29 +0800

    修正 vite.config.js base 路徑設定

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/baccarat-webapp/', // ⚠️ 這裡要填你的 GitHub 倉庫名稱前面加上斜線
   plugins: [react()],
 })
+
